e2e85e08b5c693780a879810117fb71f90dd0398
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-22 01:26:52 +0300

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+ALL beggins of the  Flask web_application
 """
-
 from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
