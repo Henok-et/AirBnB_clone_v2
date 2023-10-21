23fdda14a3b4ab1a051f21f89715848b04675714
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-22 01:40:46 +0300

    9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -7,6 +7,7 @@ from models import *
 from models import storage
 app = Flask(__name__)
 
+
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
@@ -15,6 +16,7 @@ def states(state_id=None):
     if state_id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
