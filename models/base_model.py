cd24e34e52e81dcb7021432b60bf34f5206d1077
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-22 00:46:38 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Contains class BaseModel
+ all of the Contains class BaseModel
 """
-
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
 from datetime import datetime
 import models
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
