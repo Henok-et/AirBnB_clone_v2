afdb08f912b6f5eab291187e89668bed0c491067
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-22 00:45:30 +0300

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
-""" holds class Amenity"""
-import models
+""" The whole  holds  of class Amenity"""
+
 from models.base_model import BaseModel, Base
+import models
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
 
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
