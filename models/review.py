0e13b337dd99c02af890fd96f24b3e69ae8a847b
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-22 00:48:52 +0300

    review.py

--- models/review.py
+++ models/review.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
-""" holds class Review"""
+""" ALL of the holds class Review"""
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
-
 
 class Review(BaseModel, Base):
     """Representation of Review """
