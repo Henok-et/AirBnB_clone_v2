ec1f8168e601b57958ae38407881cc8b52dfa1fa
TEMESGEN ABDISSA <109625337+TEMESGENABDISSA@users.noreply.github.com>
2023-10-22 00:49:36 +0300

    state.py

--- models/state.py
+++ models/state.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" holds class State"""
+""" All of the holds class State"""
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
 
 class State(BaseModel, Base):
     """Representation of state """
